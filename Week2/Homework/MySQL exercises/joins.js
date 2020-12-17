1d4ebfaedbc2d73b8dcb80b52c4051c372512b3b
Amjad habbabeh <amjad.habbabeh@gmail.com>
2020-12-17 16:36:29 +0100

    Amjad_Habbabeh/homework/week_2

---
+++ Week2/Homework/MySQL exercises/joins.js
@@ -0,0 +1,59 @@
+
+
+const util = require('util');
+const mysql = require('mysql');
+const fs=require('fs');
+const config=JSON.parse(fs.readFileSync("config_secret.json"))
+
+const connection = mysql.createConnection({
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  database: config.database
+});
+
+const execQuery = util.promisify(connection.query.bind(connection));
+
+async function seedDatabase() {
+  const names_Authors_and_Collaborators = `
+  
+    SELECT A.author_name , B.author_name AS collaborator_name FROM authors A     
+    LEFT JOIN authors B 
+    ON A.collaborator=B.author_no ;`;
+
+// the realation between Authors table and research_papers table is M-M so i need to creat a bridge table :
+    const  Authors_and_their_pubished_paper_title = `
+    SELECT A.*,R.paper_title
+  FROM authors A left join author_research_papers B on A.author_no=B.author_id
+  LEFT JOIN research_papers R
+  ON B.paper_id=R.paper_id ` ;
+
+    
+  connection.connect();
+
+  try {
+     execQuery( names_Authors_and_Collaborators,(err ,results)=>{
+      if(err) throw err;
+      console.log('names_Authors_and_Collaborators',results);
+
+    });
+    
+     execQuery( Authors_and_their_pubished_paper_title,(err ,result)=>{
+      if(err) throw err;
+      console.log('Authors_and_their_pubished_paper_title:',result);
+
+    });
+
+  } catch (error) {
+    console.error(error);
+    connection.end();
+  }
+  connection.end();
+
+}
+
+seedDatabase();
+
+
+
+
---
+++ Week2/Homework/MySQL exercises/keys.js
@@ -0,0 +1,44 @@
+const util = require('util');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'hyfuser',
+  password: 'hyfpassword',
+  database: 'userdb',
+});
+
+const execQuery = util.promisify(connection.query.bind(connection));
+
+async function seedDatabase() {
+  const CREATE_Authors_TABLE = `
+    CREATE TABLE IF NOT EXISTS Authors (
+      author_no int Primary Key,
+      author_name varchar(50), 
+      university varchar(50),
+      date_of_birth datetime,
+       h_index int,
+        gender ENUM('f','m')
+    );`;
+
+    const Add_COLUMN = `
+    ALTER TABLE Authors
+    ADD collaborator int ,
+    ADD FOREIGN KEY FK_no(Collaborator) REFERENCES Authors(author_no);
+    `
+  connection.connect();
+
+  try {
+    // call the function that returns promise
+    await execQuery( CREATE_Authors_TABLE );
+    await execQuery(Add_COLUMN);
+
+  } catch (error) {
+    console.error(error);
+    connection.end();
+  }
+
+  connection.end();
+}
+
+seedDatabase();
